4ae12f346360deea52196846e33c3f44a948943b
diegorodrguez7 <drplaybasketball17@gmail.com>
2023-03-23 09:49:46 +0000

    Complete exercise 4 Udemy Curse JQuery.

--- test-4/app4.js
+++ test-4/app4.js
@@ -1,14 +1,34 @@
-//Fadding effects
+// Fadding effects
 const element = "<h1>Diego Rodriguez</h1>";
 $("div").first().html(element);
 
-$("div").click(function () {
-  $(this).fadeOut(1000);
-  console.log($(this));
-});
+// Local variables
+let counter = 0;
+let hiddenElements = 0;
+let fader = 0.5;
 
-// Function to fadein and fade out the elements
+// Function to fadeOut the nodos
+$("div").click(function (e) {
+  $(this).fadeOut(1000, () => {
+    counter++;
+    hiddenElements++;
+    const temp = `<h3>Counter: ${counter}</h3>`;
+    $(this).html(temp);
+    $("button").text(`Show(${hiddenElements})`);
+  });
+})
+
+// Function to fadeTo the nodos.
 $("button").click(function () {
-  $("div").fadeIn(1000);
-  $("h1").fadeIn(1000);
+  // $("div").fadeIn(1000);
+  $("div").toggle(2000);
+  hiddenElements = 0;
+  $("button").text("All showing");
+  $("button").fadeTo("slow", fader, () => {
+    if (fader == 0.5) {
+      fader = 1;
+    } else {
+      fader = 0.5;
+    }
+  });
 });
