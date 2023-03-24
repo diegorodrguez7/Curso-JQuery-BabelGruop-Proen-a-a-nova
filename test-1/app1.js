503bb9bd8de05665b224e2921ad1a20218106f5e
diegorodrguez7 <drplaybasketball17@gmail.com>
2023-03-24 10:29:17 +0000

    Delete all zips

--- test-1/app1.js
+++
@@ -1,8 +0,0 @@
-$("h1").html("Esta vez soy Diego");
-
-// evento con función que al clicar cambia el texto del titulo
-
-$("h1").click(function () {
-  $("h1").html("Esta vez soy Diego");
-}).html("Click aquí");
-
