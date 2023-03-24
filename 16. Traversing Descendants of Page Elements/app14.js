a0ff1a082f594518765b66b9d140200a4c4fdcce
diegorodrguez7 <drplaybasketball17@gmail.com>
2023-03-24 11:03:51 +0000

    Complete exercise 16.

--- 16. Traversing Descendants of Page Elements/app14.js
+++ 16. Traversing Descendants of Page Elements/app14.js
@@ -1,2 +1,13 @@
 // Traversing Descendants
 
+$("#btn1").click(() => {
+  const sel = $("input").val();
+  if (sel) {
+    $(sel).css("background-color", "red");
+    $(".main").find(sel).css("color", "blue");
+  }
+});
+
+$("#btn2").click(() => {
+  $(".main div").find("button").css("background-color", "purple");
+});
